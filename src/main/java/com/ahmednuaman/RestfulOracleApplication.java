bf4d39d9d2710e375fbe2c6841120f987c3ec873
Ahmed Nuaman <ahmed@ahmednuaman.com>
2017-01-08 19:53:38 +0000

    init

---
+++ src/main/java/com/ahmednuaman/RestfulOracleApplication.java
@@ -0,0 +1,12 @@
+package com.ahmednuaman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulOracleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulOracleApplication.class, args);
+	}
+}
